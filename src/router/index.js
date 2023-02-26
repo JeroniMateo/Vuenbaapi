b941ad24692dc9d10e1bb49f93c6d0d08a2783f9
Jeroni <jmateo@cifpfbmoll.eu>
2023-02-26 19:32:07 +0100

    Vue Files Views

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import NBA from '../views/NBA.vue'
+import Nba from '../views/Nba.vue'
+import Games from '../views/Games.vue'
+import Players from '../views/Players.vue'
+import Stats from '../views/Stats.vue'
+import Teams from '../views/Teams.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +11,23 @@ const router = createRouter({
     {
       path: '/',
       name: 'nba',
-      component: NBA
+      component: Nba
+    },   {
+      path: '/games',
+      name: 'games',
+      component: Games
+    },   {
+      path: '/players',
+      name: 'players',
+      component: Players
+    },   {
+      path: '/stats',
+      name: 'stats',
+      component: Stats
+    },   {
+      path: '/Teams',
+      name: 'teams',
+      component: Teams
     }
   ]
 })
